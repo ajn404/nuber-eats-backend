93614ded513d325ce4287da31d14922ccf7b307b
mac_ajn404 <huiyuening00@gmail.com>
2023-05-09 11:29:28 +0800

    use class-validator class-transformer

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,19 @@
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsEmpty, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
   @Field((type) => Boolean, { nullable: true })
+  @IsEmpty()
   isVegan?: boolean;
   @Field((type) => String, { nullable: true })
+  @IsEmpty()
   address?: string;
   @Field((type) => String, { nullable: true })
+  @IsEmpty()
   ownerName?: string;
 }
