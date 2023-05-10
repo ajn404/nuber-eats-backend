846db536d9690b4164ddc99a006e958ede0d1ae7
mac_ajn404 <huiyuening00@gmail.com>
2023-05-10 16:25:26 +0800

    setup typeORM and cross env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,36 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: joi.object({
+        NODE_ENV: joi.string().valid('dev', 'prod', 'test').required(),
+        DB_HOST: joi.string().required(),
+        DB_PORT: joi.string().required(),
+        DB_USERNAME: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_DATABASE: joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      //   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,12 +1,31 @@
+/**
+ * 餐厅类
+ */
 import { Field, ObjectType } from '@nestjs/graphql';
+
 @ObjectType()
 export class Restaurant {
+  /**
+   * 餐厅名称
+   */
   @Field((type) => String, { nullable: true })
   name: string;
+
+  /**
+   * 是否为素食餐厅
+   */
   @Field((type) => Boolean, { nullable: true })
   isVegan?: boolean;
+
+  /**
+   * 餐厅地址
+   */
   @Field((type) => String, { nullable: true })
   address?: string;
+
+  /**
+   * 餐厅老板姓名
+   */
   @Field((type) => String, { nullable: true })
   ownerName?: string;
 }
